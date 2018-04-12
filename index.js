4c64ac30a829fbf3cac9b4b848bc7d5398c14783
Michael Stranges <strangesm@gmail.com>
2018-04-12 20:01:11 +0000

    completed recursion exercise

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+
+
 function sumItems(array) {
+  let mySum = 0;
   // Sum all the numbers in the array
+  array.forEach(function(num){
+    if(Array.isArray(num)){
+      mySum += sumItems(num)
+    } else {
+      mySum += num;
+    }
+  });
+return mySum;
 }
+
+console.log(sumItems([1, 2, 3, 4, 5]));
+console.log(sumItems([[1, 2, [[3], 4]], 5, []]));
+
 
 module.exports = sumItems;
